dff2d06e2c203f608954433646b0682db705fbc2
lia Müller <muellli6@students.zhaw.ch>
2025-02-26 11:11:47 +0100

    Namen geändert

--- Start.py
+++ Start.py
@@ -8,8 +8,8 @@ st.title("Meine erste Streamlit App")
 # Streamlit über den Text unten direkt in die App - cool!
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Lia Müller (muellli6@students.zhaw.ch)
+- Selina Rüdisüli (ruedisel@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
