5db18738d607305ed653261b8274ad428f149da9
SelinaPat <ruedisel@students.zhaw.ch>
2025-03-05 13:51:41 +0100

    6 änderung

--- Start.py
+++ Start.py
@@ -4,21 +4,21 @@ import pandas as pd
 st.title("Kalorienrechner")
 
 st.write("Diese App wurde entwickelt, um den täglichen Kalorienverbrauch zu berechnen. Die Berechnung basiert auf dem Grundumsatz (BMR) und dem Gesamtumsatz (TDEE).")
-# Auswahl der Seite
-page = st.radio("Wählen Sie eine Seite:", ["Startseite", "Berechnung"])
 
-if page == "Startseite":
-    # Startseite mit einem Button, um zur Berechnungsseite zu navigieren
-    st.write("Willkommen zum Kalorienrechner!")
-    
-    if st.button('Zur Berechnung'):
-        # Seite wechseln (Durch Neuladen mit einer neuen Auswahl im Radio-Button)
-        st.session_state.page = 'Berechnung'
-        st.experimental_rerun()
+# Überprüfe, ob die Berechnungsseite aufgerufen werden soll
+if 'berechnen' not in st.session_state:
+    st.session_state.berechnen = False
 
-elif page == "Berechnung":
+# Wenn der Benutzer auf den Button klickt, zur Berechnungsseite wechseln
+if st.button('Zur Berechnung'):
+    st.session_state.berechnen = True
+    st.experimental_rerun()
+
+# Wenn der Benutzer auf der Berechnungsseite ist
+if st.session_state.berechnen:
     # Berechnungsseite (Kalorienrechner)
     st.write("Berechnen Sie Ihre täglichen Kalorien!")
+
 """
 Diese App wurde von folgenden Personen entwickelt:
 - Lia Müller (muellli6@students.zhaw.ch)
